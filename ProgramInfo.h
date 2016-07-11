de563ed1b4c80a7ea3f420e422d2fcbc05647954
saikro <saikro@gmail.com>
2016-07-11 18:56:40 +0300

    The results of the inspection.

--- ProgramInfo.h
+++ ProgramInfo.h
@@ -11,14 +11,18 @@ public:
 
 	static ProgramInfo* Instance()
 	{
+		// (alex.t): Why is inst copared with 0?
+		// Is it good to compare a pointer with 0?
 		if (inst == 0)
 		{
+			// (alex.t): Why is () used here?
 			inst = new ProgramInfo();
 			return inst;
 		}
 		return inst;
 	}
 
+	// (alex.t): How to return "0.0.1" in a more effective way?
 	std::string getVersion() { return ProgramVersion; }
 
 private:
@@ -26,6 +30,8 @@ private:
  	~ProgramInfo();
 	static ProgramInfo* inst;
 
+	// (alex.t): Why have you decided to use
+	// std::string to store "0.0.1"?
 	std::string ProgramVersion = "0.0.1";
 
 };
